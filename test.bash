773a44eb73a12e96889e84e00355cc8561e4c02a
okowa42 <a.hirohara.0526@gmail.com>
2024-11-07 11:53:52 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,13 +1,24 @@
-#!/bin/bash
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2024 Akari Hirohara
+# SPDX=License=Identifier: BSD-3-Clause
 
 ng () {
 	echo ${1}行目が違うよ
 	res=1
 }
 
-rea=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"
-[ "$a" = 山田 ] || ng "$LINENO"
+res=0
 
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+out=$(echo あ | ./plus)
+[ "$?" = 1] || ng "LINENO"
+[ "${out}" = "" ] ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng "LINENO"
+[ "${out}" = "" ] || ng "LINENO"
+
+[ "${res}" = 0 ] && echo OK
 exit $res
